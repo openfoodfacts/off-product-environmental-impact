f4b17814199e429f4286cb1d3fa1246fa042438e
Gustave Coste <gustavecoste@gmail.com>
2020-11-18 17:17:12 +0100

    Minor improvement

--- ingredients_characterization/characterize_ingredients.py
+++ ingredients_characterization/characterize_ingredients.py
@@ -1,4 +1,10 @@
 """ Script to run all the OFF ingredients characterization script at one and in the right order """
+import os
+
+from data import INGREDIENTS_DATA_FILEPATH
+
+if os.path.isfile(INGREDIENTS_DATA_FILEPATH):
+    os.remove(INGREDIENTS_DATA_FILEPATH)
 
 from ingredients_characterization.nutrition.ciqual import nutri_from_ciqual
 from ingredients_characterization.nutrition.fcen import nutri_from_fcen
