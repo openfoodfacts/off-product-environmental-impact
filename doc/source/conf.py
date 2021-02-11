0fc100f3db937dbd8d5746381c9c5f4fcd207af6
Gustave Coste <gustavecoste@gmail.com>
2021-02-11 14:29:31 +0100

    Added the readme page of the project in the doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,8 +42,10 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
+    'sphinxcontrib.napoleon',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    "m2r2",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -52,8 +54,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -79,7 +81,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
