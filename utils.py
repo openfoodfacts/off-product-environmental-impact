43571e82154d05a3d7750dd289706bb0d1f0e42b
Gustave Coste <gustavecoste@gmail.com>
2021-05-26 15:55:11 +0200

    Merged utils.py and reporting/utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,44 @@
 import json
+from pathlib import Path
 
 import requests
+
+
+def ensure_extension(filename, extension):
+    """
+        Ensures of the extension of a file
+
+        >>> ensure_extension('foo.pdf', 'pdf')
+        'foo.pdf'
+        >>> ensure_extension('bar.csv', 'pdf')
+        'bar.pdf'
+    """
+    return str(Path(filename).with_suffix(f".{extension}"))
+
+
+def smart_round_format(number, precision):
+    """
+    Args:
+        number (float):
+        precision (int):
+
+    Returns:
+        str:
+
+    Examples:
+        >>> smart_round_format(258.658, 2)
+        '258.66'
+        >>> smart_round_format(0.258658, 2)
+        '0.26'
+        >>> smart_round_format(0.0000258658, 2)
+        '2.59e-05'
+    """
+
+    if number >= 0.1:
+        return str(round(number, 2))
+
+    else:
+        return ('{:0.' + str(precision) + 'e}').format(number)
 
 
 def get_product_from_barcode(barcode):
