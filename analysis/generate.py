2e39bfafe017d544bb86d5cd563b790eff8b8d12
Martin Bruse <zond@google.com>
2021-10-05 16:08:15 +0200

    Generating random combos instead of iterating if there are too many combinations.

--- analysis/generate.py
+++ analysis/generate.py
@@ -4,19 +4,28 @@ import json
 import sys
 import copy
 import multiprocessing
-
+import numpy as np
+import traceback
 
 PARALLELLISM = 64
-MAX_INGREDIENT_COMBOS = 4
-
+SEED = 1
 DONE = "DONE"
+COMBOS_PER_PRODUCT = 16.0
 
-def print_now(s):
-    print(s)
-    sys.stdout.flush()
+np.random.seed(SEED)
+
+def indexed_combo(prod, percentages, unicorns):
+    cpy = copy.deepcopy(prod)
+    for idx, known in enumerate(percentages):
+        if not known:
+            del cpy['ingredients'][idx]['percent']
+    for idx, unicorn in enumerate(unicorns):
+        if unicorn:
+          cpy['ingredients'][idx]['id'] = 'en:unicorn-meat'
+    return cpy
 
 def all_percentage_combinations(prod, switch_index=0):
-    if switch_index == len(prod['ingredients']) - 1 or switch_index == MAX_INGREDIENT_COMBOS:
+    if switch_index == len(prod['ingredients']) - 1:
         yield prod
         cpy = copy.deepcopy(prod)
         del cpy['ingredients'][switch_index]['percent']
@@ -28,28 +37,44 @@ def all_percentage_combinations(prod, switch_index=0):
         yield from all_percentage_combinations(cpy, switch_index=switch_index+1)
 
 def all_unicorn_combinations(prod, switch_index=0):
-    if switch_index == len(prod['ingredients']) - 1 or switch_index == MAX_INGREDIENT_COMBOS:
+    if switch_index == len(prod['ingredients']) - 1:
         yield prod
         cpy = copy.deepcopy(prod)
-        cpy['ingredients'][switch_index]['id'] = 'en:unicorn-feces'
+        cpy['ingredients'][switch_index]['id'] = 'en:unicorn-meat'
         yield cpy
     else:
         yield from all_unicorn_combinations(prod, switch_index=switch_index+1)
         cpy = copy.deepcopy(prod)
-        cpy['ingredients'][switch_index]['id'] = 'en:unicorn-feces'
+        cpy['ingredients'][switch_index]['id'] = 'en:unicorn-meat'
         yield from all_unicorn_combinations(cpy, switch_index=switch_index+1)
 
+def push_prod(iterated, n, num, prod, product_queue):
+  known = len(list(filter(lambda i: "en:unicorn-meat" != i["id"], prod["ingredients"])))
+  if known > 0:
+      print(f'{n}/{num} {prod["product_name"]} (iterated {iterated})\n * percentages: {list(map(lambda i: "percent" in i, prod["ingredients"]))}\n * known: {list(map(lambda i: "en:unicorn-meat" != i["id"], prod["ingredients"]))}', flush=True)
+      product_queue.put(prod)
+
 def generate_combos(products, product_queue):
-    n = 1
-    for prod in products:
-        for percentage_combo in all_percentage_combinations(prod):
-            for unicorn_combo in all_unicorn_combinations(percentage_combo):
-                known = len(list(filter(lambda i: "en:unicorn-feces" != i["id"], unicorn_combo["ingredients"])))
-                if known > 0:
-                    print_now(f'{n}/{len(products)} {prod["product_name"]}\n * percentages: {list(map(lambda i: "percent" in i, unicorn_combo["ingredients"]))}\n * known: {list(map(lambda i: "en:unicorn-feces" != i["id"], unicorn_combo["ingredients"]))}')
-                    product_queue.put(unicorn_combo)
-        n = n + 1
-    product_queue.put(DONE)
+    try:
+        n = 1
+        for prod in products:
+            combos = 2.0 ** (len(prod["ingredients"]) * 2)
+            likelihood_per_combo = COMBOS_PER_PRODUCT / combos
+            if likelihood_per_combo > 0.005:
+                for percentage_combo in all_percentage_combinations(prod):
+                    for unicorn_combo in all_unicorn_combinations(percentage_combo):
+                        if np.random.rand() < likelihood_per_combo:
+                            push_prod(True, n, len(products), unicorn_combo, product_queue)
+            else:
+                for _ in range(int(COMBOS_PER_PRODUCT)):
+                    combo = indexed_combo(prod,
+                                          np.random.choice([True, False], size=[len(prod["ingredients"])]).tolist(),
+                                          np.random.choice([True, False], size=[len(prod["ingredients"])]).tolist())
+                    push_prod(False, n, len(products), combo, product_queue)
+            n = n + 1
+        product_queue.put(DONE)
+    except Exception as e:
+        print(traceback.format_exception(None, e, e.__traceback__), file=sys.stderr, flush=True)
 
 def queue_all(queue):
     while True:
@@ -59,36 +84,42 @@ def queue_all(queue):
         yield val
 
 def save_results(result_queue, done_queue):
-    with open('generated_test_data.json', 'w') as f:
-        f.write('[')
-        for result in queue_all(result_queue):
-            json.dump(result, f, indent=2, sort_keys=True)
-            f.write(',\n')
-        f.write(']\n')
-    done_queue.put(DONE)
+    try:
+        with open('generated_test_data.json', 'w') as f:
+            f.write('[')
+            for result in queue_all(result_queue):
+                json.dump(result, f, indent=2, sort_keys=True)
+                f.write(',\n')
+            f.write(']\n')
+        done_queue.put(DONE)
+    except Exception as e:
+        print(traceback.format_exception(None, e, e.__traceback__), file=sys.stderr, flush=True)
 
 def estimate_products(product_queue, result_queue):
-    impact_categories = ['EF single score',
-            'Climate change']
-    for prod in queue_all(product_queue):
-        result = {
-                'ground_truth': prod,
-                }
-        try:
-            impact_estimation_result = estimate_impacts(
-                    seed=1,
-                    product=prod,
-                    distributions_as_result=True,
-                    impact_names=impact_categories)
-            result['estimation'] = impact_estimation_result
-        except Exception as e:
-            result['error'] = f'{e.__class__.__name__}'
-        result_queue.put(result)
-    result_queue.put(DONE)
+    try:
+        impact_categories = ['EF single score',
+                'Climate change']
+        for prod in queue_all(product_queue):
+            result = {
+                    'ground_truth': prod,
+                    }
+            try:
+                impact_estimation_result = estimate_impacts(
+                        seed=1,
+                        product=prod,
+                        distributions_as_result=True,
+                        impact_names=impact_categories)
+                result['estimation'] = impact_estimation_result
+            except Exception as e:
+                result['error'] = f'{e.__class__.__name__}'
+            result_queue.put(result)
+        result_queue.put(DONE)
+    except Exception as e:
+        print(traceback.format_exception(None, e, e.__traceback__), file=sys.stderr, flush=True)
 
 
 def main():
-    print_now('Loading all ground truth objects...')
+    print('Loading all ground truth objects...', flush=True)
     with open('test_dataset_nutri_from_ciqual.json') as f:
         ground_truth = json.load(f)
 
@@ -99,9 +130,9 @@ def main():
     for i in range(PARALLELLISM):
         multiprocessing.Process(target=estimate_products, args=(product_queue, result_queue)).start()
 
-    print_now('Generating data...')
+    print('Generating data...', flush=True)
     generate_combos(ground_truth, product_queue)
-    print(done_queue.get())
+    print(done_queue.get(), flush=True)
 
 if __name__ == '__main__':
     main()
